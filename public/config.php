2c20fb1ad4d92a42e42b8641b483fe6c14a09111
JUN <soipon0@gmail.com>
2019-05-04 15:53:10 +0900

    TodoAppのスタイル付与

---
+++ public/config.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('display_errors', 1);
+
+define('DSN', 'mysql:host=localhost;dbname=testdb');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', 'password');
--- public/index.php
+++ public/index.php
@@ -1,3 +1,37 @@
 <?php
 
-echo "Hello from TodoApp";
+require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '/functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>TodoApp</title>
+</head>
+<body>
+    <div id="container">
+        <h1>Todos</h1>
+        <form action="">
+            <input type="text" id="new_todo" placeholder="What needs to be done?">
+        </form>
+        <ul>
+            <li>
+                <input type="checkbox">
+                <span>Do something!</span>
+                <div class="delete_todo">x</div>
+            </li>
+            <li>
+                <input type="checkbox" checked>
+                <span class="done">Do something again!</span>
+                <div class="delete_todo">x</div>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
